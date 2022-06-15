29ee9b5930105a9d56dae2a98bbef218614b59af
Nikolai Makarevich <nikoladze529@mail.ru>
2022-06-15 23:46:17 +0300

    feat: add Server class

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import http from 'http';
+import 'dotenv/config';
+import * as os from 'os';
+import Handler from './utils/handler';
+
+class Server {
+  PORT = process.env.SERVER_PORT || 5000;
+
+  server!: http.Server;
+
+  handler!: Handler;
+
+  run() {
+    this.server = http.createServer((req, res) => {
+      this.handler = new Handler();
+      this.handler.handleReq(req, res);
+    });
+    this.server.listen(this.PORT, () => {
+      process.stdout.write(`Server running on port ${this.PORT}${os.EOL}`);
+    });
+  }
+}
+
+const server = new Server();
+server.run();
