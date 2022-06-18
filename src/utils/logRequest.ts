09df6b9f3777c88bdc216b57b48ba79c00901146
Nikolai Makarevich <nikoladze529@mail.ru>
2022-06-18 13:30:57 +0300

    fix: fix eslint errors

--- src/utils/logRequest.ts
+++ src/utils/logRequest.ts
@@ -2,12 +2,13 @@ import http from 'http';
 import * as os from 'os';
 
 const logRequest = (req: http.IncomingMessage, statusCode: number) => {
-  const coloredStatusCode =
-    Math.floor(statusCode / 100) === 2
-      ? `\x1b[32m${statusCode}\x1b[0m`
-      : Math.floor(statusCode / 100) === 4
-      ? `\x1b[33m${statusCode}\x1b[0m`
-      : `\x1b[31m${statusCode}\x1b[0m`;
+  let coloredStatusCode = `\x1b[31m${statusCode}\x1b[0m`;
+  if (Math.floor(statusCode / 100) === 2) {
+    coloredStatusCode = `\x1b[32m${statusCode}\x1b[0m`;
+  }
+  if (Math.floor(statusCode / 100) === 4) {
+    coloredStatusCode = `\x1b[33m${statusCode}\x1b[0m`;
+  }
   process.stdout.write(
     `${req.method} ${req.url} ${coloredStatusCode}${os.EOL}`
   );
